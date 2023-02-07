18002c45435b456f24279729fb147f66ff682978
I hate JAVA <f@example.com>
2023-02-07 17:04:38 +0800

    2023.02.07 first !

---
+++ src/main/java/com/once/officialsite/OnceOfficialsiteApplication.java
@@ -0,0 +1,13 @@
+package com.once.officialsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnceOfficialsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnceOfficialsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/once/officialsite/OnceOfficialsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.once.officialsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnceOfficialsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
